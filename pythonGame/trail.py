03caca7de8ca4def1b08be71d5c65a6dcf5895e0
DesertedVoltron <36799888+DesertedVoltron@users.noreply.github.com>
2023-01-27 03:14:02 -0500

    Delete pythonGame directory

--- pythonGame/trail.py
+++
@@ -1,9 +0,0 @@
-import os, pyglet
-
-
-class trail:
-    def __init__(self, initial_position, image = pyglet.resource.image('sprites/enemy.png')):
-        self.image = image
-        self.pos = initial_position
-        self.trails = []
-    
